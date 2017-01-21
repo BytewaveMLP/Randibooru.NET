3d51550fd1667f341455f814ca345b55deb1f21a
Eliot Partridge <shortcircuit369@gmail.com>
2017-01-21 13:26:09 -0600

    Start using command line options

--- Randibooru/Randibooru.cs
+++ Randibooru/Randibooru.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using Discord;
+using Discord.Net;
 using Discord.Commands;
 using CommandLine;
 
 namespace Randibooru {
 	class Randibooru {
 		private DiscordClient _client;
+		private Options options;
 
 		static void Main(string[] args) {
 			var options = new Options();
 			if (Parser.Default.ParseArguments(args, options)) {
-				new Randibooru().Start();
+				new Randibooru(options).Start();
 			}
+		}
+
+		private Randibooru(Options options) {
+			this.options = options;
 		}
 
 		public void Start() {
@@ -33,13 +39,22 @@ namespace Randibooru {
 			});
 
 			_client.ExecuteAndWait(async () => {
-				await Console.Out.WriteLineAsync("Logging in to Discord...");
-				await _client.Connect("MjQ5MzIzMDI3MzY2NTQzMzYw.C1nIAQ.txMT7UObHJPCtlMYkf5yKj1xu0M", TokenType.Bot);
-				await Console.Out.WriteLineAsync("Success!");
-				Profile user = _client.CurrentUser;
-				await Console.Out.WriteLineAsync("-------------------");
-				await Console.Out.WriteLineAsync("USER: @" + user.Name + "#" + user.Discriminator);
-				await Console.Out.WriteLineAsync("ID:   " + user.Id);
+
+				try {
+					await _client.Connect(this.options.DiscordAPIKey, TokenType.Bot);
+				} catch (HttpException e) {
+					await Console.Error.WriteLineAsync("[FATAL]: " + e.Message);
+					await Console.Error.WriteLineAsync("Ensure your Discord API key is correct, and check");
+					await Console.Error.WriteLineAsync("    https://status.discordapp.com");
+					await Console.Error.WriteLineAsync("for any outages.");
+					Environment.Exit(127);
+				}
+				await Console.Out.WriteLineAsync("Successfully logged into Discord!");
+				if (this.options.Verbose) {
+					Profile user = _client.CurrentUser;
+					await Console.Out.WriteLineAsync("USER: @" + user.Name + "#" + user.Discriminator);
+					await Console.Out.WriteLineAsync("ID:   " + user.Id);
+				}
 			});
 		}
 	}
